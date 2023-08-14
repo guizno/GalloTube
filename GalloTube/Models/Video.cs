3da693f8bda32f1830a5c694b27f6095fba0facb
Guilherme Henrique <guizinho.zaza14@gmail.com>
2023-08-14 09:35:34 -0300

    Iniciando o Projeto GalloTube

---
+++ GalloTube/Models/Video.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace GalloTube.Models;
+
+[Table("Video")]
+public class Video
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Display(Name = "Título")]
+    [Required(ErrorMessage = "O Título é obrigatório")]
+    [StringLength(100, ErrorMessage = "O Título deve possuir no máximo 100 caracteres")]
+    public string Title { get; set; }
+
+    [Display(Name = "Descrição")]
+    [Required(ErrorMessage = "A Descrição é obrigatória")]
+    [StringLength(8000, ErrorMessage = "A Descrição deve possuir no máximo 5000 caracteres")]
+    public string Description { get; set; }
+
+    [Column(TypeName = "DATETIME")]
+    [Display(Name = "Data de Upload")]
+    [Required(ErrorMessage = "A Data de Upload é obrigatório")]
+    public Int16 UploadDate { get; set; }
+
+    [Display(Name = "Duração (em minutos)")]
+    [Required(ErrorMessage = "A Duração é obrigatória")]
+    public Int16 Duration { get; set; }
+
+    [StringLength(200)]
+    [Display(Name = "Foto")]
+    public string Image { get; set; }
+
+    [NotMapped]
+    [Display(Name = "Duração")]
+    public string HourDuration { get {
+        return TimeSpan.FromMinutes(Duration) .ToString(@"%h'h 'mm'min'");
+    }}
+
+    public ICollection<TagVideo> Tags { get; set; }
+}
